aac04c322da2162bb6de0232665014bb8611b9b0
Kevin O'Neill <oneillkeving@gmail.com>
2020-12-09 11:20:49 -0500

    Added full design-matrix to GP model

--- gp/gp.r
+++ gp/gp.r
@@ -24,24 +24,29 @@ data_grid <- function(df, xmin=-600, xmax=600, xsize=10,
 }
 
 bin_space <- function(df, xmin=-600, xmax=600, xsize=10,
-                      ymin=-300, ymax=300, ysize=10) {
-    ## initialize an empty grid (including the existing grouping structure)
-    df.grid <- data_grid(df, xmin=xmin, xmax=xmax, xsize=xsize,
-                         ymin=ymin, ymax=ymax, ysize=ysize)
+                      ymin=-300, ymax=300, ysize=10,
+                      grid=data_grid(df, xmin=xmin, xmax=xmax, xsize=xsize,
+                                     ymin=ymin, ymax=ymax, ysize=ysize)) {
+    ## find break points from bin centers
+    x_bins <- grid %>% pull(bin_x) %>% unique
+    y_bins <- grid %>% pull(bin_y) %>% unique
+    x_breaks <- x_bins[-length(x_bins)] + xsize/2
+    y_breaks <- y_bins[-length(y_bins)] + ysize/2
     
     ## bin df according to the grid
     df %>%
-        mutate(bin_x=xsize*round(CURRENT_FIX_X/xsize),
-               bin_y=ysize*round(CURRENT_FIX_Y/ysize)) %>%
+        mutate(bin_x=x_bins[findInterval(CURRENT_FIX_X, x_breaks) + 1],
+               bin_y=y_bins[findInterval(CURRENT_FIX_Y, y_breaks) + 1]) %>%
         group_by(bin_x, bin_y, .add=TRUE) %>%
         summarize(count=n()) %>%
-        right_join(df.grid) %>%
+        right_join(grid) %>%
         mutate(count=replace_na(count, 0)) %>%
         return()
 }
 
 
-data.fix <- read.csv('../data/test/KKTest1_BlankFixations_report.csv', header=TRUE) %>%
+data.fix <- read.csv('../data/test/KKTest1_BlankFixations_report.csv',
+                     header=TRUE) %>%
     mutate(CURRENT_FIX_X = CURRENT_FIX_X - 1920/2,
            CURRENT_FIX_Y = CURRENT_FIX_Y - 1080/2) %>%
     group_by(TRIAL_INDEX) %>%
@@ -49,122 +54,151 @@ data.fix <- read.csv('../data/test/KKTest1_BlankFixations_report.csv', header=TR
            PREV_FIX_Y = lag(CURRENT_FIX_Y)) %>%
     ungroup()
 
+## generate fake subjects/conditions
 data.groups <- data.frame(TRIAL_INDEX=unique(data.fix$TRIAL_INDEX),
-                          SUB=c(1, 1, 1, 2, 2,
-                                1, 2, 1, 1, 2,
-                                1, 2, 2, 1, 2,
-                                2, 2, 1))
+                          PAR=as.factor(c(1, 1, 1, 2, 2,
+                                          1, 2, 1, 1, 2,
+                                          1, 2, 2, 1, 2,
+                                          2, 2, 1))) %>%
+    group_by(PAR) %>%
+    mutate(COND=as.factor(rep(1:3, 3)))
 
-data.fix <- data.fix %>% mutate(SUB=data.groups$SUB[TRIAL_INDEX])
+data.fix <- data.fix %>% mutate(PAR=data.groups$PAR[TRIAL_INDEX],
+                                COND=data.groups$COND[TRIAL_INDEX])
 
 ggplot(data.fix) +
-    aes(x=CURRENT_FIX_X, y=CURRENT_FIX_Y, color=as.factor(SUB)) +
+    aes(x=CURRENT_FIX_X, y=CURRENT_FIX_Y, color=as.factor(PAR)) +
     geom_point() +
-    coord_cartesian(c(-600, 600), c(-300, 300)) +
+    facet_wrap(PAR ~ COND) +
+    coord_cartesian(c(-450, 500), c(-150, 300)) +
     theme_bw() + theme(aspect.ratio=0.5)
 
-
 ggplot(data.fix) +
-    aes(x=CURRENT_FIX_X, y=CURRENT_FIX_Y, color=as.factor(SUB)) +
+    aes(x=CURRENT_FIX_X, y=CURRENT_FIX_Y, color=as.factor(PAR)) +
     geom_point() +
     facet_wrap(~ TRIAL_INDEX) +
     coord_cartesian(c(-600, 600), c(-300, 300)) +
     theme_bw() + theme(aspect.ratio=0.5)
 
-plot.data <- data.fix %>%
-    group_by(SUB, TRIAL_INDEX) %>%
-    bin_space(xsize=150, ysize=150) %>%
-    group_by(SUB, bin_x, bin_y) %>%
-    summarize(count=mean(count)) %>%
-    ggplot() + ggtitle('Raw Data') +
-    aes(x=bin_x, y=bin_y, fill=count) + xlab('X') + ylab('Y') +
-    geom_raster() +
-    facet_wrap(~ SUB) +
-    scale_fill_viridis(option='magma', name='Rate') +
-    theme_classic() + theme(aspect.ratio=0.5)
 
 
 data.grid <- data.fix %>%
-    data_grid(xsize=150, ysize=150) %>%
-    select(-grid_idx)
+    data_grid(xmin=-450, xmax=550, xsize=50,
+              ymin=-200, ymax=300, ysize=50)
+
 data.binned <- data.fix %>%
-    group_by(SUB, TRIAL_INDEX) %>%
-    bin_space(xsize=150, ysize=150)
+    group_by(PAR, COND, TRIAL_INDEX) %>%
+    bin_space(xmin=-450, xmax=550, xsize=50,
+              ymin=-200, ymax=300, ysize=50)
+
+plot.data.par <- data.binned %>%
+    group_by(COND, PAR, bin_x, bin_y) %>%
+    summarize(count=mean(count)) %>%
+    ggplot(aes(x=bin_x, y=bin_y, fill=count)) +
+    geom_raster() +
+    facet_grid(COND ~ PAR, labeller=label_both) +
+    scale_fill_viridis(option='magma', name='Rate', limits=c(0, 4)) +
+    theme_classic() + theme(aspect.ratio=0.5)
+
+plot.data.group <- data.binned %>%
+    group_by(COND, bin_x, bin_y) %>%
+    summarize(count=mean(count)) %>%
+    ggplot(aes(x=bin_x, y=bin_y, fill=count)) +
+    geom_raster() +
+    facet_grid(COND ~ ., labeller=label_both) +
+    scale_fill_viridis(option='magma', name='Rate', limits=c(0, 2.5)) +
+    theme_classic() + theme(aspect.ratio=0.5)
 
 
-
-data.stan <- list(D=2,  ## 2 dimensions (X, Y)
-                  N=nrow(data.binned),
+design.matrix <- model.matrix(~ COND, data.binned)
+data.pred <- expand(data.grid, nesting(grid_idx, bin_x, bin_y),
+                    COND=unique(data.fix$COND))
+data.stan <- list(N=nrow(data.binned),
                   G=nrow(data.grid),
-                  P=length(unique(data.binned$SUB)),
-                  p=data.binned$SUB,
+                  P=length(unique(data.binned$PAR)),
+                  D=2,  ## 2 dimensions (X, Y)
+                  K=ncol(design.matrix),
+                  M=ncol(design.matrix),
+                  C=3,
                   g=data.binned$grid_idx,
-                  x=data.grid,
-                  y=data.binned$count)
+                  p=as.integer(data.binned$PAR),
+                  c=as.integer(data.binned$COND),
+                  grid=select(data.grid, bin_x, bin_y),
+                  X=design.matrix,
+                  Z=design.matrix,
+                  y=data.binned$count,
+                  Npred=nrow(data.pred),
+                  gpred=data.pred$grid_idx,
+                  cpred=as.integer(data.pred$COND),
+                  Xpred=model.matrix(~ COND, data.pred))
 
 
 gp.fit <- readRDS('gp.rds')
 
-gp.fit <- stan(file='gp.stan', data=data.stan, chains=1)
+gp.fit <- stan(file='gp.stan', data=data.stan, chains=2)
 
 saveRDS(gp.fit, 'gp.rds')
 
 
-print(gp.fit, prob=c(0.025, 0.5, 0.975),
-      pars=c('m_intercept', 'sd_intercept', 'intercept',
-             'rho', 'rho_p', 'alpha', 'alpha_p'))
-plot(gp.fit, pars=c('m_intercept', 'sd_intercept', 'intercept',
-                    'rho', 'rho_p', 'alpha', 'alpha_p'))
-pairs(gp.fit, pars=c('m_intercept', 'sd_intercept', 'intercept',
-                     'rho', 'rho_p', 'alpha', 'alpha_p'))
+print(gp.fit, prob=c(0.025, 0.5, 0.975), 
+      pars=c('a', 'beta', 'sigma','gamma',
+             'rho', 'rho_tilde', 'alpha', 'alpha_tilde'))
+
+plot(gp.fit, pars=c('a', 'beta', 'sigma', 'gamma'))
+pairs(gp.fit, pars=c('sigma', 'gamma'))
+
+## extract model fits for each trial
+draws.trial <- spread_draws(gp.fit, lambda[.row], yhat[.row]) %>%
+    bind_cols(., data.binned[.$.row, c('PAR', 'COND', 'bin_x', 'bin_y', 'grid_idx')])
+
+## extract group-level model fits for each condition
+draws.group <- spread_draws(gp.fit, lambda_group[.row], yhat_group[.row]) %>%
+    rename(lambda=lambda_group,
+           yhat=yhat_group) %>%
+    bind_cols(., data.pred[.$.row, c('COND', 'bin_x', 'bin_y', 'grid_idx')])
 
 
-p <- tidy_draws(gp.fit)
-
-pred <- p %>% select(.chain, .iteration, .draw,
-                     starts_with('lambda'), starts_with('yhat')) %>%
-    pivot_longer('lambda[1]':paste0('yhat[', data.stan$N,']'),
-                 names_to=c('parameter', '.row'),
-                 names_pattern='([[:alnum:]]+)\\[([[:digit:]]+)\\]') %>%
-    mutate(.row=as.integer(.row)) %>%
-    cbind(., data.binned[.$.row, c('SUB', 'TRIAL_INDEX',
-                                  'bin_x', 'bin_y', 'grid_idx')])  %>%
-    pivot_wider(names_from='parameter') %>%
-    full_join(select(p, .chain, .iteration, .draw))
-
-
-plot.pred <- pred %>%
-    group_by(SUB, bin_x, bin_y) %>%
-    mean_hdi(lambda) %>%
+plot.pred.par <- draws.trial %>%
+    group_by(PAR, COND, bin_x, bin_y) %>%
+    median_hdci(lambda) %>%
     ggplot() + ggtitle('GP Fit (by subject)') + 
     aes(x=bin_x, y=bin_y) + xlab('X') + ylab('Y') +
     geom_raster(aes(fill=exp(lambda))) +
-    facet_wrap(~ SUB) +
-    scale_fill_viridis(option='magma', name='Rate') +
+    facet_grid(COND ~ PAR, labeller=label_both) +
+    scale_fill_viridis(option='magma', name='Rate', limits=c(0, 4)) +
     theme_classic() + theme(aspect.ratio=0.5)
-plot.data / plot.pred  + plot_layout(guides='collect')
-ggsave('plots/gp/gp_fit.png')
+
+(plot.data.par | plot.pred.par) + plot_layout(guides='collect')
+ggsave('plots/gp_fit.png')
 
 
-group_pred <- p %>% select(.chain, .iteration, .draw, m_intercept, starts_with('f[')) %>%
-    pivot_longer('f[1]':paste0('f[', data.stan$G, ']'),
-                 names_to=c('parameter', '.row'),
-                 names_pattern='([[:alnum:]]+)\\[([[:digit:]]+)\\]') %>%
-    mutate(.row=as.integer(.row)) %>%
-    bind_cols(., data.grid[.$.row,]) %>%
-    pivot_wider(names_from='parameter') %>%
-    full_join(select(p, .chain, .iteration, .draw))
-
-group_pred %>%
-    group_by(bin_x, bin_y) %>%
-    mean_hdi(lambda=f) %>%
-    arrange(desc(lambda))
-    
+plot.pred.group <- draws.group %>%
+    group_by(COND, bin_x, bin_y) %>%
+    median_hdci(lambda) %>%
     ggplot() + ggtitle('GP Fit (group)') + 
     aes(x=bin_x, y=bin_y) + xlab('X') + ylab('Y') +
+    facet_grid(COND ~ ., labeller=label_both) +
     geom_raster(aes(fill=exp(lambda))) +
-    scale_fill_viridis(option='magma', name='Rate') +
+    scale_fill_viridis(option='magma', name='Rate') + ##, limits=c(0, 2.5)) +
     theme_classic() + theme(aspect.ratio=0.5)
 
-ggsave('plots/gp/gp_group_fit.png')
+(plot.data.group | plot.pred.group) + plot_layout(guides='collect')
+ggsave('plots/gp_group_fit.png')
 
+
+
+
+## Group-level contrasts
+contr.group <- draws.group %>%
+    group_by(COND, bin_x, bin_y) %>%
+    compare_levels(lambda, by=COND)
+
+contr.group %>% median_hdci() %>%
+    ggplot(aes(x=bin_x, y=bin_y, fill=lambda)) +
+    facet_grid(COND ~ .) +
+    geom_raster(aes(fill=lambda)) +
+    scale_fill_viridis(option='magma', name='Rate') +
+    theme_classic() + theme(aspect.ratio=0.5)
+    
+contr.group %>% median_hdci() %>%
+    filter(.upper < 0 | .lower > 0)
--- gp/gp.stan
+++ gp/gp.stan
@@ -2,15 +2,15 @@ functions {
   /*
    * compute a latent Gaussian process
    * args:
-   *   x: the GP predictors
+   *   grid: the GP predictors
    *   alpha: the gp marginal SD parameter
    *   rho: the length-scales for each dimension of x
    *   eta: independent standard normal variates
    * returns:
    *   a vector of GP predictions at predictor locations x
    */
-  vector gp_exp_quad_ARD(vector[] x, real alpha, vector rho, vector eta) {
-    int N = size(x);
+  vector gp_exp_quad_ARD(vector[] grid, real alpha, vector rho, vector eta) {
+    int N = size(grid);
     real delta = 1e-8;
     matrix[N, N] K;
     real alpha_sq = square(alpha);
@@ -18,7 +18,7 @@ functions {
     for (i in 1:(N-1)) {
       K[i, i] = alpha_sq + delta;
       for (j in (i + 1):N) {
-        K[i, j] = alpha_sq * exp(-0.5 * dot_self((x[i] - x[j]) ./ rho));
+        K[i, j] = alpha_sq * exp(-0.5 * dot_self((grid[i] - grid[j]) ./ rho));
         K[j, i] = K[i, j];
       }
     }
@@ -31,74 +31,123 @@ data {
   int<lower=1> N;     // total number of data points
   int<lower=1> G;     // number of data points per trial (grid size)
   int<lower=1> P;     // number of participants
-  int<lower=1> D;     // number of dimensions
+  int<lower=1> D;     // number of GP input dimensions
+  int<lower=1> K;     // number of population-level effects
+  int<lower=1> M;     // number of participant-level effects
+  int<lower=1> C;     // number of conditions
 
   int<lower=1, upper=G> g[N];  // grid index for each trial
   int<lower=1, upper=P> p[N];  // participant index for each trial
+  int<lower=1, upper=C> c[N];  // condition index for each trial
 
-  vector[D] x[G];     // input data (same across trials)
+  vector[D] grid[G];  // input data (same across trials)
+  matrix[N, K] X;     // population-level design matrix
+  matrix[N, M] Z;     // participant-level design matrix
   int<lower=0> y[N];  // output data (counts)
+
+  // group-level predictions
+  int<lower=0> Npred;
+  int<lower=1, upper=G> gpred[Npred];
+  int<lower=1, upper=C> cpred[Npred];
+  matrix[Npred, K] Xpred;
+}
+
+transformed data {
+  int Kc = K - 1;
+  matrix[N, Kc] Xc;    // centered version of X without an intercept
+  vector[Kc] means_X;  // column means of X before centering
+  matrix[Npred, Kc] Xpred2 = Xpred[,2:K];
+
+  for (i in 2:K) {
+    means_X[i - 1] = mean(X[, i]);
+    Xc[, i - 1] = X[, i] - means_X[i - 1];
+  }
 }
 
 parameters {
-  // Population-level parameters 
-  real m_intercept;               // population intercept
-  real<lower=0> sd_intercept;     // sd of individual intercepts
-  vector[G] eta;                  // group-level GP mean variates
+  // Population-level parameters
+  real ac;                        // temporary intercept for centered predictors
+  vector[Kc] beta;                // population-level effects
+  vector<lower=0>[M] sigma;       // sds of participant-level effects
+  cholesky_factor_corr[M] L;      // correlation matrix of participant-level effects
+  vector[G] eta[C];               // population-level GP mean variates (by condition)
 
   // Participant-level parameters
-  real z_intercept[P];            // standardized intercepts
-  vector[G] eta_p[P];             // GP mean variates
+  matrix[M, P] z;                 // standardized group-level effects
+  vector[G] eta_tilde[C, P];      // participant-level GP mean variates
 
   // Hyperparameters
-  vector<lower=0>[D] rho;         // group-level length-scale
-  real<lower=0> alpha;            // group-level marginal standard deviation
-  vector<lower=0>[D] rho_p;       // participant-level length-scale
-  real<lower=0> alpha_p;          // participant-level marginal standard deviation
+  vector<lower=0>[D] rho;         // population-level length-scale
+  real<lower=0> alpha;            // population-level marginal standard deviation
+  vector<lower=0>[D] rho_tilde;   // participant-level length-scale
+  real<lower=0> alpha_tilde;      // participant-level marginal standard deviation
 }
 
 transformed parameters {
-  vector[G] f;                    // group-level GP
-  real intercept[P];              // actual participant-level intercepts
-  vector[G] f_p[P];               // participant-level GPs
+  vector[G] f[C];                 // group-level GPs (by condition)
+  matrix[P, M] gamma;             // actual participant-level effects
+  vector[G] f_tilde[C, P];        // participant-level GPs (by participantXcondition)
 
-  f = gp_exp_quad_ARD(x, alpha, rho, eta);
-  for (i in 1:P) {
-    intercept[i] = m_intercept + sd_intercept * z_intercept[i];
-    f_p[i] = gp_exp_quad_ARD(x, alpha_p, rho_p, eta_p[i]);
+  gamma = transpose(diag_pre_multiply(sigma, L) * z);
+  
+  for (i in 1:C) {
+    f[i] = gp_exp_quad_ARD(grid, alpha, rho, eta[i]);
+    for (j in 1:P) {
+      f_tilde[i, j] = gp_exp_quad_ARD(grid, alpha_tilde, rho_tilde, eta_tilde[i, j]);
+    }
   }
 }
 
 model {
   vector[N] lambda;
   for (i in 1:N) {
-    lambda[i] = intercept[p[i]] + f[g[i]] + f_p[p[i], g[i]];
+    lambda[i] = ac + Xc[i]*beta + f[c[i], g[i]] +   // population-level effects
+		dot_product(Z[i], gamma[p[i]]) +    // participant-level effects
+		f_tilde[c[i], p[i], g[i]];      
   }
 
   // Population-level parameters
-  // *improper uniform prior for m_intercept*
-  sd_intercept ~ std_normal();
-  eta ~ std_normal();
+  // *improper uniform prior for ac*
+  beta ~ student_t(3, 0, 1);
+  sigma ~ student_t(3, 0, 2.5);
+  L ~ lkj_corr_cholesky(1);
+  for (i in 1:C) {
+    eta[i] ~ std_normal();
+    for (j in 1:P) {
+      eta_tilde[i, j] ~ std_normal();
+    }
+  }
 
   // Participant-level parameters
-  z_intercept ~ std_normal();
-  for (i in 1:P) { eta_p[i] ~ std_normal(); }
+  to_vector(z) ~ std_normal();
 
   // Hyperparameters
   rho ~ inv_gamma(10, 500);
   alpha ~ std_normal();
-  rho_p ~ inv_gamma(20, 600);
-  alpha_p ~ std_normal();
+  rho_tilde ~ inv_gamma(20, 600);
+  alpha_tilde ~ std_normal();
   
   y ~ poisson_log(lambda);
 }
 
 generated quantities {
+  // obtain the true intercept (for non-centered design matrices)
+  real a = ac - dot_product(means_X, beta);
   vector[N] lambda;
+  vector[Npred] lambda_group;
   int yhat[N];
+  int yhat_group[Npred];
   
+  // trial-level predictions
   for (i in 1:N) {
-    lambda[i] = intercept[p[i]] + f[g[i]] + f_p[p[i], g[i]];
+    lambda[i] = ac + Xc[i]*beta + f[c[i], g[i]] +
+		dot_product(Z[i], gamma[p[i]]) + f_tilde[c[i], p[i], g[i]];      
   }
   yhat = poisson_log_rng(lambda);
+
+  // group-level predictions
+  for (i in 1:Npred) {
+    lambda_group[i] = a + Xpred2[i]*beta + f[cpred[i], gpred[i]];
+  }
+  yhat_group = poisson_log_rng(lambda_group);
 }
